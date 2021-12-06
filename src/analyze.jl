e8ec59f2831418ecf3d1734afad2110cc377c29f
Uziel Linares <uziel.linares@protonmail.com>
2021-12-05 19:01:40 -0600

    replace get_scores for a more general function get_date_metrics

--- src/analyze.jl
+++ src/analyze.jl
@@ -1,6 +1,6 @@
-function get_scores(report_response::AbstractArray)
+function get_date_metrics(report_response::AbstractArray, metric_name::String)
     dates = Date.([item["summary_date"] for item in report_response], "y-m-d")
-    scores = [get(item, "score", 0) for item in report_response]
+    scores = [get(item, metric_name, 0) for item in report_response]
 
     indices = sortperm(dates)
     dates = dates[indices]
